f183808b1c111d95520e7402a5a1fd4b9438e999
Rafael Campos <rafa.campos.200.rc@gmail.com>
2025-04-17 00:16:11 -0300

    refactor: adjust migration

--- application/src/main/resources/db/migrations/V5__create_evaluation_table.sql
+++ application/src/main/resources/db/migrations/V5__create_evaluation_table.sql
@@ -1,9 +1,8 @@
 CREATE TABLE evaluation (
   id BINARY(16) PRIMARY KEY DEFAULT (UUID()),
   submission_id BINARY(16) UNIQUE REFERENCES submission(id),
-  detected_pattern_id BINARY(16) REFERENCES design_pattern(id),
-  correctness_score DECIMAL(5,2), -- Exemplo: 85.00%
-  style_score DECIMAL(5,2), -- Exemplo: 75.00%
+  pattern_id BINARY(16) REFERENCES design_pattern(id),
+  score DECIMAL(5,2),
   feedback TEXT,
   evaluated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
