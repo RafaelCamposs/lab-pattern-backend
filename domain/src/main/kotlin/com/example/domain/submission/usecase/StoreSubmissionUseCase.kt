a8b47c736e6a4a7b1f5652abffc3d75316021d2a
Rafael Campos da Paixao <rafa.campos.200.rc@gmail.com>
2025-08-21 21:20:33 -0300

    Revert "refactor: removing user entity"
    This reverts commit e9e6ca86ad67b839f4d98f3fad8d9d655172e447.
    
    # Conflicts:
    #	application/src/main/kotlin/com/example/application/user/entrypoint/rest/UserController.kt
    #	domain/src/main/kotlin/com/example/domain/submission/usecase/StoreSubmissionUseCase.kt

--- domain/src/main/kotlin/com/example/domain/submission/usecase/StoreSubmissionUseCase.kt
+++ domain/src/main/kotlin/com/example/domain/submission/usecase/StoreSubmissionUseCase.kt
@@ -6,18 +6,21 @@ import com.example.domain.pattern.gateway.GetPatternByIdGateway
 import com.example.domain.submission.entity.Submission
 import com.example.domain.submission.entity.dto.StoreSubmissionDto
 import com.example.domain.submission.gateway.StoreSubmissionGateway
+import com.example.domain.user.gateway.GetUserByIdGateway
 import jakarta.inject.Named
 
 @Named
 class StoreSubmissionUseCase (
     private val storeSubmissionGateway: StoreSubmissionGateway,
     private val getChallengeByIdGateway: GetChallengeByIdGateway,
+    private val getUserByIdGateway: GetUserByIdGateway,
     private val getPatternByIdGateway: GetPatternByIdGateway,
     private val storeEvaluationUseCase: StoreEvaluationUseCase,
 ) {
     fun execute(storeSubmissionDto: StoreSubmissionDto): Result<Submission> {
         return runCatching {
             val challenge = getChallengeByIdGateway.execute(storeSubmissionDto.challengeId).getOrThrow()
+            val user = getUserByIdGateway.execute(storeSubmissionDto.userId).getOrThrow()
             val pattern = getPatternByIdGateway.execute(storeSubmissionDto.patternId).getOrThrow()
             val submission = storeSubmissionGateway.execute(storeSubmissionDto).getOrThrow()
             val evaluation = storeEvaluationUseCase.execute(
